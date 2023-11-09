cba0d434d0507f631a5dcdb44ae4cd471b170a66
alexandre.dias <alexandrechavesdias@hotmail.com>
2023-11-09 16:14:05 -0300

    ajuste controller

---
+++ exercicios/src/main/java/fiap/com/exercicios/controller/teste.java
@@ -0,0 +1,16 @@
+package fiap.com.exercicios.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+@RestController
+@RequestMapping("v1/teste")
+@RequiredArgsConstructor
+public class teste {
+    @GetMapping
+    public String listAssociates() {
+    	return "testes";
+    }
+}
